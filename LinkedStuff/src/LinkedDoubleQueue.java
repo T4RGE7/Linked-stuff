bb6cbc0e6b90578169772b51d12a709468ac2ee2
T4RGE7 <t4rge7@gmail.com>
2013-07-17 13:48:07 -0400

    Added LL stuff

--- LinkedStuff/src/LinkedDoubleQueue.java
+++ LinkedStuff/src/LinkedDoubleQueue.java
@@ -4,6 +4,24 @@
  *
  * 
  */
-public class LinkedDoubleQueue {
+public class LinkedDoubleQueue<T> {
 
+	private LinkedDoubleNode<T> head, current;
+	private int size;
+	
+	public LinkedDoubleQueue() {
+		this.head = null;
+		this.current = null;
+		this.tail = null;
+		this.size = 0;
+	}
+	
+	public LinkedDoubleQueue(T data) {
+		this.head = new LinkedDoubleNode<T>(data);
+		this.current = null;
+		this.tail = null;
+		this.size = 1;
+	}
+	
+	
 }
